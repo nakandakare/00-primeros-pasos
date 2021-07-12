7c04dbc53879ee420c1dba0f79a22cb8456d5ba2
nakandakare <kevin.nakandakare@gmail.com>
2021-07-12 19:32:42 -0300

    first commit

---
+++ funciones-flecha.js
@@ -0,0 +1,12 @@
+const sumar = (a, b) => a + b;
+
+const restar = (a, b) => a - b;
+
+const multiplicar = (a, b) => a * b;
+
+const dividir = (a, b) => a / b;
+
+console.log('suma:', sumar(2, 4));
+console.log('resta:', restar(5, 2));
+console.log('multiplicación:', multiplicar(5, 10));
+console.log('división:', dividir(10, 2));
